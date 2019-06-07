c4240c13e5cbee00cef6fd0c8e694f94d3d7cf2a
Igordr1999 <Igordr1999@yandex.ru>
2019-06-07 22:58:49 +0300

    Create lab3_2

---
+++ lab3_2/main/java/ru/igor99/lab3_2/Records.java
@@ -0,0 +1,49 @@
+package ru.igor99.lab3_2;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import ru.igor99.lab3_2.Adapter.CardAdapter;
+
+import java.util.ArrayList;
+
+public class Records extends Activity {
+
+    private RecyclerView cardRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.records);
+
+        DBProvider db_provider = DBProvider.getInstance(this);
+        SQLiteDatabase db = db_provider.getDB();
+
+        ArrayList<String> students_names = new ArrayList<String>();
+
+        Cursor c = db.rawQuery("select * from students", null);
+        if (c != null) {
+            if (c.moveToFirst()) {
+                StringBuilder sb = new StringBuilder();
+                do {
+                    sb.setLength(0);
+                    for (String cn : c.getColumnNames()) {
+                        sb.append(cn + " = "
+                                + c.getString(c.getColumnIndex(cn)) + "; ");
+                    }
+                    students_names.add(String.valueOf(sb));
+                } while (c.moveToNext());
+            }
+        }
+        c.close();
+
+        cardRecyclerView = findViewById(R.id.recycle_view);
+        CardAdapter adapter = new CardAdapter(students_names.size(), students_names);
+        cardRecyclerView.setAdapter(adapter);
+        cardRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+}
